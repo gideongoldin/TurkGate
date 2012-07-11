0a8a6dec3ffcecc3d67afba733d338cd981494a6
Gideon Goldin <gideongoldin@gmail.com>
2012-07-11 16:32:33 -0400

    Tidied up completion code files

--- TurkGate Server/generateCompletionCode.php
+++ TurkGate Server/generateCompletionCode.php
@@ -1,29 +1,20 @@
 <?php session_start(); ?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		
+	<head>	
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		
 		<title>TurkGate</title>
-	
 		<script>
-		
+			//<![CDATA[
 			function exit() {
 				return "Please verify that you have saved the code on this page before leaving!";
 			}
-			
 			window.onbeforeunload = exit;
-			
+			//]]>
 		</script>
-	
 	</head>
-
 	<body>
-
-		<?php
-			
+		<?php	
 			// Add the Worker ID and the Group name to the to-be-hashed input string.
 			$inputString = 'w[' . $_COOKIE['Worker_ID'] . ']g[' . $_COOKIE['Group_Name'] . ']';
 		
@@ -40,15 +31,12 @@
 			
 			// Display the completion code to the user
 			echo '<div style="width:50%; margin:200px auto;">';
-				echo '<p style="text-align:center; font-weight:bold;">Thank you!</p>';
-				echo '<p style="text-align:center;">Please enter the code below into the Mechanical Turk HIT page to receive credit for your participation.</p><hr />';
-				echo '<p style="text-align:center; margin-top:26px;"><span style="color:yellow; background:black; font-family:Courier, monospace; padding:10px;">';
-					echo $completionCode;
-				echo '</span></p>';
+			echo '<p style="text-align:center; font-weight:bold;">Thank you!</p>';
+			echo '<p style="text-align:center;">Please enter the code below into the Mechanical Turk HIT page to receive credit for your participation.</p><hr />';
+			echo '<p style="text-align:center; margin-top:26px;"><span style="color:yellow; background:black; font-family:Courier, monospace; padding:10px;">';
+			echo $completionCode;
+			echo '</span></p>';
 			echo '</div>';
-		
 		?>
-		
 	</body>
-	
 </html>
