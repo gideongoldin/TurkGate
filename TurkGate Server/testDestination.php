15e481a170f411a9eabecbb5d723301e56393d54
Adam Darlow <adam.darlow@gmail.com>
2012-10-26 14:29:02 -0400

    Added link to generate a completion code from testDestination.php. Also clarified the explanation of how to use testDestination.php

--- TurkGate Server/testDestination.php
+++ TurkGate Server/testDestination.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <html><body>
 <!--
    Copyright 2012 Adam Darlow and Gideon Goldin
@@ -15,27 +17,69 @@
    limitations under the License.
 
 -->
-<!-- This page can be used to test whether SurveyForm.php sends you where you intend and what variables are set. In order to reach this page through SurveyForm.php, add it to the SurveySites database along with an identifier (e.g. "test"). If you then go to SurveyForm.php with the parameter "survey=test 1", it will send you to this page. The "1" is arbitrary but required. SurveyForm expects a specific survey identifier but this page doesn't care what it is. -->
-	<p>You have reached your destination. Congratulations.</p>
-	<p>Here are the parameters you sent:</p>
+<!-- This page can be used to test whether SurveyAccess.php sends you where you intend and what variables are set. In order to reach this page through SurveyForm.php, add it to the SurveySites database along with an identifier (e.g. "test"). If you then go to SurveyForm.php with the parameter "survey=test 1", it will send you to this page. The "1" is arbitrary but required. SurveyForm expects a specific survey identifier but this page doesn't care what it is. -->
+	<p>TurkGate let you through to this page. If you try again with the same worker ID and group it should stop you.</p>
+	
+	<p>To test completion code generation, click the link below. In order to include a variable in the completion code, first enter its name and value and click Submit.</p>
+ 	<?php
+		
+		if(isset($_POST['submit']))
+		{
+		    $varName = urlencode($_POST['varName']);
+		    $varValue = urlencode($_POST['varValue']);
+		    echo "<p><a href = 'generateCompletionCode.php?$varName=$varValue'>Completion code</a></p>";
+		}
+		else 
+		{
+			echo "<p><a href = 'generateCompletionCode.php'>Completion code</a></p>";
+		}
+	?>
+	<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+		<table>
+			<tr>
+	   			<td>
+	   				Name:
+	   			</td>
+	   			<td>
+		   			<input type="text" name="varName">
+	   			</td>
+		   </tr>
+			<tr>
+	   			<td>
+	   				Value:
+	   			</td>
+	   			<td>
+		   			<input type="text" name="varValue">
+	   			</td>
+		   </tr>
+	   </table>
+	   <input type="submit" name="submit" value="Submit"><br>
+	</form> 	 	
+	<br><br>
+	<p>Below is a log of the system state (for debugging purposes).</p>
 	<ul>
 		<?php		
-		session_start();
 		
 		// print all URL parameters
+		echo '<li>URL variables:<ul>';
 		foreach ($_GET as $key => $value) {
 			echo "<li> URL: $key = $value </li>";
 		}
+		echo "</ul></li>";
 		
 		// print all cookies
+		echo "<li>Cookies:<ul>";
 		foreach ($_COOKIE as $key => $value) {
 			echo "<li> COOKIE: $key = $value </li>";
 		}
+		echo "</ul></li>";
 		
 		// print all session variables
+		echo "<li>Session variables:<ul>";
 		foreach ($_SESSION as $key => $value) {
 			echo "<li> SESSION: $key = $value </li>";
 		}
+		echo "</ul></li>";
 		?>
 	</ul>
     <!-- You can add a link to the completion code here to test it as well. 
