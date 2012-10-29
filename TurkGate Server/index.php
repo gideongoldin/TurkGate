6b31649c0e7911566c8ea434c3d6c27c8e20438c
Adam Darlow <adam.darlow@gmail.com>
2012-10-29 17:59:07 -0400

    Added form for testing TurkGate.

--- TurkGate Server/index.php
+++ TurkGate Server/index.php
@@ -1,8 +1,20 @@
-<?php
+<?php session_start();
+
 // Get TurkGate's database configuration
 if (!include ('turkGateConfig.php')) {
     header('Location: install.php');
     exit();
+}
+
+// If the form was submitted, test survey access
+if (isset($_POST['submit'])) {
+    $workerID = urlencode($_POST['workerID']);
+    $groupName = urlencode($_POST['groupName']);
+	$surveyUrl = urlencode($_POST['surveyUrl']);
+	$source = urlencode($_POST['source']);
+
+    header("Location: surveyAccess.php?assignmentId=test&workerId=$workerID&group=$groupName&survey=$surveyUrl");
+	exit();
 }
 ?>
 <!DOCTYPE HTML>
@@ -14,6 +26,44 @@ if (!include ('turkGateConfig.php')) {
   	<h1>TurkGate</h1>
   	<h2>Installation</h2>
     <p>TurkGate has already been installed!</p>
+    <br>
+    <h2>Testing</h2>
+    <p>To test the installation, enter a workerId and group name below and click Test.</p>
+      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+      <table>
+        <tr>
+          <td> Worker Id: </td>
+          <td>
+          <input type="text" name="workerID" value="1">
+          </td>
+        </tr>
+        <tr>
+          <td> Group: </td>
+          <td>
+          <input type="text" name="groupName" value="test group">
+          </td>
+        </tr>
+      </table>
+      <input type="submit" name="submit" value="Test">
+      <br>
+      <p>If you have a specific survey to test, you can enter its URL below.</p>
+      <table>
+        <tr>
+          <td> Survey URL: </td>
+          <td>
+          <input type="text" name="surveyURL" value="test">
+          </td>
+        </tr>
+      </table>
+      <p>
+      	Also specify whether you plan to create an external HIT or use the 
+      	Javascript template.
+      </p>
+      <input type="radio" name="source" value="ext">External HIT<input/>
+      <input type="radio" name="source" value="js">Javascript<input/>
+      <input type="submit" name="submit" value="Test">
+    </form>
+
     <h5>
       &copy; 2012 
       <a href='https://github.com/gideongoldin/TurkGate'>TurkGate</a>
