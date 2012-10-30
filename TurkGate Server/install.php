1baa58a2f5ef5e6e9cb3d5e6a040a225ba98a8ee
Gideon Goldin <gideongoldin@gmail.com>
2012-10-30 17:32:20 -0400

    Updated install script to prompt for database name

--- TurkGate Server/install.php
+++ TurkGate Server/install.php
@@ -10,16 +10,15 @@
 
 	// Check for form submissions
 	if (isset($_POST['submit'])) {
-		// Database name is hard-coded
-		$database_name = "TurkGate";
-
+		
 		// Gather the user-submitted database credentials
 		$database_host = isset($_POST['database_host']) ? $_POST['database_host'] : "";
+		$database_name = isset($_POST['database_name']) ? $_POST['database_name'] : "";
 		$database_username = isset($_POST['database_username']) ? $_POST['database_username'] : "";
 		$database_password = isset($_POST['database_password']) ? $_POST['database_password'] : "";
 
 		// Validate entries
-		if (empty($database_host) || empty($database_username) || empty($database_username) || empty($database_password)) {
+		if (empty($database_host) || empty($database_name) || empty($database_username) || empty($database_password)) {
 			// For HTMLn where n < 5
 			echo "<h1>TurkGate</h1><h2>Installation</h2><p>Error: All fields are required. Please <a href='javascript:history.back()'>go back</a> and re-submit.</p>" . $footer;
 		} else {
@@ -40,34 +39,28 @@
 			fwrite($configFileHandle, $configFileString);
 			fclose($configFileHandle);
 
-			// Create the TurkGate database
-			if (mysql_query("CREATE DATABASE IF NOT EXISTS " . $database_name, $connection)) {
-				// Select the database
-				mysql_select_db($database_name, $connection) or die("<p>Error selecting database: " . mysql_error() . "</p><p><a href='javascript:history.back()'>Go back</a></p>" . $footer);
+			// Select the database
+			mysql_select_db($database_name, $connection) or die("<h1>TurkGate</h1><h2>Installation</h2><p>Error selecting database: " . mysql_error() . "</p><p><a href='javascript:history.back()'>Go back</a></p>" . $footer);
 
-				// Create the table
-				$sql = "CREATE TABLE IF NOT EXISTS SurveyRequest 
-			(
-				requestID INT NOT NULL AUTO_INCREMENT,
-				PRIMARY KEY(requestID),
-				workerID VARCHAR(256),
-				URL VARCHAR(256),
-				groupName VARCHAR(256),
-				time DATETIME
-			)";
+			// Create the table
+			$sql = "CREATE TABLE IF NOT EXISTS SurveyRequest 
+		(
+			requestID INT NOT NULL AUTO_INCREMENT,
+			PRIMARY KEY(requestID),
+			workerID VARCHAR(256),
+			URL VARCHAR(256),
+			groupName VARCHAR(256),
+			time DATETIME
+		)";
 
-				mysql_query($sql, $connection) or die("<p>Error creating table: " . mysql_error() . "</p><p><a href='javascript:history.back()'>Go back</a></p>" . $footer);
-				mysql_close($connection);
+			mysql_query($sql, $connection) or die("<p>Error creating table: " . mysql_error() . "</p><p><a href='javascript:history.back()'>Go back</a></p>" . $footer);
+			mysql_close($connection);
 
-				// Installation complete
-				echo "<h1>TurkGate</h1><h2>Installation</h2><p>TurkGate Installation successful!</p>";
-				echo "<p>You may now close this window.</p>" . $footer;
-				exit();
-			} else {
-				echo "<p>Error creating database. (Might it already exist?): " . mysql_error();
-				echo "<p>A configuration file has been created in the TurkGate server directory.</p>" . $footer;
-				exit();
-			}
+			// Installation complete
+			echo "<h1>TurkGate</h1><h2>Installation</h2><p>TurkGate Installation successful!</p>";
+			echo "<p>You may now close this window.</p>" . $footer;
+			exit();
+			
 		}
 		exit();
 	}
@@ -91,10 +84,16 @@
 				<input type="text" name="database_host" value="localhost" required="required">
 				<i>(Note: this value is usually 'localhost')</i>
 			</p>
+			
+			<p>
+				<label for="database_name">Database Name:</label>
+				<input type="text" name="database_name" required="required" autofocus="autofocus">
+				<i>(E.g., 'TurkGate')</i>
+			</p>
 
 			<p>
 				<label for="database_username">Database Username:</label>
-				<input type="text" name="database_username" autofocus="autofocus" required="required">
+				<input type="text" name="database_username" required="required">
 			</p>
 
 			<p>
