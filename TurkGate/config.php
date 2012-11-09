b6a3e2408eb50cdfd612605cd96428916d45963e
Adam Darlow <adam.darlow@gmail.com>
2012-11-09 10:41:09 -0500

    Removing config.php. Added it accidentally.

--- TurkGate/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-					define('DATABASE_HOST', 'localhost');
-					define('DATABASE_NAME', 'turkgate');
-					define('DATABASE_USERNAME', 'root');
-					define('DATABASE_PASSWORD', 'vegetable');
-					define('BASE_URL', 'http://turkgate.localhost');
-					define('KEY', '7e3a6f7211b5268fcad90b2a881c3443ff6e6739');
-						?>
