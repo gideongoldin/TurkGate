6d95b7bbfb94e72a3b835dc0cad410d84dee05e4
Gideon Goldin <gideongoldin@gmail.com>
2012-11-15 17:44:59 -0500

    Updated styling

--- TurkGate/includes/footer.php
+++ TurkGate/includes/footer.php
@@ -1,19 +1,17 @@
-			<hr />
-			
-			<div class="row">
-				<div class="eight columns">
+			<div class="sixteen columns clearfix" style="border-top: 1px solid #DDD; padding-top:10px;">
+				<div class="eight columns alpha">
 					<h6>
 					  Powered by <a href='http://gideongoldin.github.com/TurkGate/'>TurkGate</a>.
 					</h6>	
 				</div>
-				<div class="eight columns">
+				<div class="eight columns omega">
+					<footer>
 					<h6 style="text-align: right;">
-					  Copyright &copy; Adam Darlow & Gideon Goldin. All rights reserved.</h6>	
+					  Copyright &copy; Adam Darlow and Gideon Goldin. All rights reserved.
+					</h6>
+					</footer>	
 				</div>
-				
-			</div>
-
-		</div><!-- container -->
-
+			</div> <!-- sixteen columns clearfix -->
+		</div> <!-- container -->
 	</body>
 </html>
--- TurkGate/index.php
+++ TurkGate/index.php
@@ -88,101 +88,106 @@
 			$webTemplateString = preg_replace('/<!--[^>]*-->/', $copyright, $webTemplateString, 1);
 		}
 	}   
-?><!-- Import the header -->
+?>
+
+<!-- Import the header -->
 <?php require_once('includes/header.php'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
-<html>
-	<head>
-		<title></title>
-	</head>
-	<body>
-		<div class="container">
-			<div class="sixteen columns">
-				<h1 class="remove-bottom" style="margin-top: 40px">
-					TurkGate
-				</h1>
-				<hr>
-				<h3>
-					Generate a HIT
-				</h3>
+
+	<div class="container"> <!-- Container -->
+		
+		<div class="sixteen columns">
+		<header>
+			<h1 class="remove-bottom" style="margin-top: 40px; padding-bottom: 10px;">TurkGate</h1>
+		</header>
+		</div>		
+		
+		
+			<div class="sixteen columns clearfix" style="border-top: 1px solid #DDD; padding-top:10px;"> <!-- sixteen columns clearfix -->
 				<form method="post" action="index.php" id="hitGenerationForm" name="hitGenerationForm">
-					<fieldset>
-						<div class="row">
-							<div class="six columns alpha">
+				<h3>Generate a HIT</h3>
+				<div class="six columns alpha">
+					<p>
+						From here you may generate the HTML code for your Web Interface HIT, or download files for use with the Command Line Tool.
+					</p>
+					<p>
+						Please specify a survey URL and group name:
+					</p>
+					<p>
+						<label for="externalSurveyURL">*External Survey URL:</label> <input type="text" name="externalSurveyURL" value='<?php echo "$externalSurvey"; ?>' size="40" placeholder="http://surveysite.com/surveyid" autofocus="" required="">
+					</p>
+					<p>
+						<label for="groupName">*Group Name:</label> <input type="text" name="groupName" value='<?php echo "$groupName"; ?>' size="40" placeholder="Test group name" required="">
+					</p>
+				</div>
+				
+				<div class="ten columns omega"> <!-- Ten columns omega -->
+					<div id="tabs"> <!-- Tabs -->
+						
+						<!-- Tab headers -->
+						<ul>
+							<li>
+								<a href="#tabs-1">Web Interface</a>
+							</li>
+							<li>
+								<a href="#tabs-2">Command Line Tools</a>
+							</li>
+						</ul>
+						
+						<div id="tabs-1"> <!-- 1st Tab content -->
 							<p>
-								From here you may generate the HTML code for your Web Interface HIT, or download files for use with the Command Line Tool.
+								Generate the HTML code to paste into your HIT using the values specified above. Full instructions are on the <a href="http://gideongoldin.github.com/TurkGate/" target="blank">TurkGate Wiki</a>.
 							</p>
-								<p>
-									Please specify a survey URL and group name:
-								</p>
-								<p>
-									<label for="externalSurveyURL">*External Survey URL:</label> <input type="text" name="externalSurveyURL" value='<?php echo "&#39;$externalSurveyURL&#39;"; ?>' size="40" placeholder="http://surveysite.com/surveyid" autofocus="" required="">
-								</p>
-								<p>
-									<label for="groupName">*Group Name:</label> <input type="text" name="groupName" value='<?php echo "&#39;$groupName&#39;"; ?>' size="40" placeholder="Test group name" required="">
-								</p>
-							</div>
-							
-							<div class="ten columns omega">
-								<div id="tabs">
-									<ul>
-										<li>
-											<a href="#tabs-1">Web Interface</a>
-										</li>
-										<li>
-											<a href="#tabs-2">Command Line Tools</a>
-										</li>
-									</ul>
-									<div id="tabs-1">
-										<p>
-											Generate the HTML code to paste into your HIT using the values specified above. Full instructions are on the <a href="http://gideongoldin.github.com/TurkGate/" target="blank">TurkGate Wiki</a>.
-										</p>
-										<p>
-											<input type="submit" name="generateHTMLCode" id="generateHTMLCode" value="Generate HTML code">
-										</p><?php
-																		// Generate a text area with the HTML code
-																		if(strlen($webTemplateString) > 0) {
-																			echo '<p>Copy and paste the code below into the source code for your HIT:';
-																			echo '<p><textarea rows="15" cols="80" id="generatedHTMLCode">';
-																			echo $webTemplateString;
-																			echo '</textarea></p>';
-																		}
-																	?>
-									</div>
-									<div id="tabs-2">
-										<p>
-											Download the files for creating your HIT using the values specified above. Full instructions are on the <a href="https://github.com/gideongoldin/TurkGate/wiki/Command-Line-Tools" target="blank">TurkGate Wiki</a>.
-										</p>
-										<h4>Download:</h4>
-										<p>
-											<input type="submit" name="downloadCLTFile" value="survey.input"> <input type="submit" name="downloadCLTFile" value="survey.properties"> <input type="submit" name="downloadCLTFile" value="survey.question">
-										</p>
-									</div>
-								</div>
-							</div> <!-- 10 columns -->
-						</div> <!-- row -->
-					</fieldset>
+							<p>
+								<input type="submit" name="generateHTMLCode" id="generateHTMLCode" value="Generate HTML code">
+							</p>
+							<?php
+								// Generate a text area with the HTML code
+								if(strlen($webTemplateString) > 0) {
+									echo '<p>Copy and paste the code below into the source code for your HIT:';
+									echo '<p><textarea rows="15" cols="80" id="generatedHTMLCode">';
+									echo $webTemplateString;
+									echo '</textarea></p>';
+								}
+							?>
+						</div> <!-- 1st Tab content -->
+						
+						<div id="tabs-2"> <!-- 2nd Tab content -->
+							<p>
+								Download the files for creating your HIT using the values specified above. Full instructions are on the <a href="https://github.com/gideongoldin/TurkGate/wiki/Command-Line-Tools" target="blank">TurkGate Wiki</a>.
+							</p>
+							<h4>Download:</h4>
+							<p>
+								<input type="submit" name="downloadCLTFile" value="survey.input"> <input type="submit" name="downloadCLTFile" value="survey.properties"> <input type="submit" name="downloadCLTFile" value="survey.question">
+							</p>
+						</div> <!-- 2nd Tab content -->
+					</div> <!-- Tabs -->
+				</div> <!-- Ten columns omega -->
 				</form>
-				<hr>
-				<h3>
-					Completion Codes
-				</h3>
-				<p>
-					To automatically generate completion codes at the end of your surveys, redirect your workers to the following URL: <a href="#"><?php echo constant('BASE_URL'); ?>/codes/generate.php</a>.
-				</p>
-				<p>
-					Click <a href="codes/verify.php">here</a> to verify completion codes.
-				</p>
-				<p>
-					Visit the TurkGate Wiki pages on <a href="https://github.com/gideongoldin/TurkGate/wiki/Completion-Code-Generation" target="blank">Completion code generation</a> and <a href="https://github.com/gideongoldin/TurkGate/wiki/Completion-Code-Verification" target="blank">Completion code verification</a> for more information.
-				</p>
-			</div><!-- Custom jQuery actions -->
-			<script type="text/javascript">
-$(document).ready(function() {
-			$("#tabs").tabs();
-			});
-			</script> <!-- Import the footer -->
-			<?php require_once('includes/footer.php'); ?>
+			</div> <!-- sixteen columns clearfix -->
+		
+			
+		<div class="sixteen columns" style="border-top: 1px solid #DDD; padding-top:10px;">	
+		<h3>
+			Completion Codes
+		</h3>
+		<p>
+			To automatically generate completion codes at the end of your surveys, redirect your workers to the following URL: <a href="#"><?php echo constant('BASE_URL'); ?>/codes/generate.php</a>.
+		</p>
+		<p>
+			Click <a href="codes/verify.php">here</a> to verify completion codes.
+		</p>
+		<p>
+			Visit the TurkGate Wiki pages on <a href="https://github.com/gideongoldin/TurkGate/wiki/Completion-Code-Generation" target="blank">Completion code generation</a> and <a href="https://github.com/gideongoldin/TurkGate/wiki/Completion-Code-Verification" target="blank">Completion code verification</a> for more information.
+		</p>
 		</div>
-	</body>
-</html>
+	
+		<!-- Custom jQuery actions -->
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$("#tabs").tabs();
+			});
+		</script> 
+
+		<!-- Import the footer -->
+		<?php require_once('includes/footer.php'); ?>
+
