37b024f596c9f1a6f83dfac68d763219c28c8e92
unknown <Adam@.(none)>
2012-12-11 12:30:04 -0500

    Don't change test URL.

--- TurkGate/lib/fixhttp.lib.js
+++ TurkGate/lib/fixhttp.lib.js
@@ -1,4 +1,8 @@
 function fix_http(url_str) {
+	  if (url_str == 'test') {
+	  	return url_str;
+	  }
+	  
     if (url_str.length > 0 && url_str.indexOf("http") != 0) {
         url_str = "http://" + url_str;
     }
