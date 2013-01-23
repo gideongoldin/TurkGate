d03635d81f166ef3b52e6b99cce0e5bd8a6273ba
Gideon Goldin <gideongoldin@gmail.com>
2013-01-23 17:42:48 -0500

    Finished UI changes.

--- TurkGate/index.php
+++ TurkGate/index.php
@@ -30,45 +30,6 @@
 ?>
 <script src="lib/fixhttp.lib.js"></script>
 		
-<<<<<<< HEAD
-	<div class="sixteen columns clearfix" style="border-top: 1px solid #ccc; padding-top:10px;"> <!-- sixteen columns clearfix -->
-		<form method="post" id="hitGenerationForm" name="hitGenerationForm">
-		<h3>Generate a HIT</h3>
-		<div class="six columns alpha">
-			<p>
-				From here you may generate the HTML code for your Web Interface HIT, or download files for use with the Command Line Tool.
-			</p>
-			<p>
-				<label for="HITType">HIT Type:</label>
-				<select name="HITType">
-  					<option value="webInterface">Web Interface</option>
-  					<option value="CLT">Command Line Tools</option>
-				</select>
-			</p>
-			<p>
-				<label for="surveySites">External Survey Site:</label>
-				<select name="surveySites">
-  					<option value="LimeSurvey">LimeSurvey</option>
-  					<option value="Qualtrics" selected>Qualtrics</option>
-  					<option value="Other">Other Sites / Custom</option>
-				</select>
-			</p>
-			<p>
-				<label for="externalSurveyURL">External Survey URL:</label>
-				<input type="text" name="externalSurveyURL" id="externalSurveyURL" value='' size="40" placeholder="http://surveysite.com/surveyid" autofocus="" required="">
-			</p>
-			<p>
-				<label for="groupName">Group Name:</label>
-				<span class="comment">Previously created group names will appear below</span>
-				<input type="text" name="groupName" id="groupName" value='' size="40" placeholder="Test group name" required="" class="adjacent">				
-			</p>
-=======
-<div class="sixteen columns">
-  <header>
-	<h1 class="remove-bottom"><a href="index.php">TurkGate</a></h1>
-  </header>
-</div>		
-
 	<div class="sixteen columns clearfix" id="containerMain"> <!-- sixteen columns clearfix -->
 		<div class="eight columns alpha" id="leftColumnMain">
 			<form method="post" id="hitGenerationForm" name="hitGenerationForm">
@@ -78,14 +39,14 @@
 				</p>
 				<p>
 					<label for="HITType">HIT Type:</label>
-					<select name="HITType">
-	  					<option value="webInterface">Web Interface</option>
+					<select name="HITType" id="HITType">
+	  					<option value="WebInterface">Web Interface</option>
 	  					<option value="CLT">Command Line Tools</option>
 					</select>
 				</p>
 				<p>
 					<label for="surveySites">External Survey Site:</label>
-					<select name="surveySites">
+					<select name="surveySites" id="surveySites">
 	  					<option value="LimeSurvey">LimeSurvey</option>
 	  					<option value="Qualtrics" selected>Qualtrics</option>
 	  					<option value="Other">Other Sites / Custom</option>
@@ -93,12 +54,12 @@
 				</p>
 				<p>
 					<label for="externalSurveyURL">External Survey URL:</label>
-					<input type="text" name="externalSurveyURL" id="externalSurveyURL" value='' size="40" placeholder="http://surveysite.com/surveyid" autofocus="">
+					<input type="text" required name="externalSurveyURL" id="externalSurveyURL" value='' size="40" placeholder="http://surveysite.com/surveyid" autofocus="">
 				</p>
 				<p>
 					<label for="groupName">Group Name:</label>
 					<span class="comment">Previously created group names will appear below</span>
-					<input type="text" name="groupName" id="groupName" value='' size="40" placeholder="Test group name" class="adjacent">				
+					<input type="text" required name="groupName" id="groupName" value='' size="40" placeholder="Test group name" class="adjacent">				
 				</p>
 				<p>
 					<label for="associatedURLs">Survey URLs in Group:</label>
@@ -111,19 +72,23 @@
 		</div>	
 	
 		<div class="eight columns omega" id="rightColumnMain"> <!-- columns omega -->
->>>>>>> Added sliding drawer in index.php.
-			<p>
-				Generate the HTML code to paste into your HIT. Full instructions are on the <a href="http://gideongoldin.github.com/TurkGate/" target="blank">TurkGate Wiki</a>.
-			</p>
+
+			<div id="WebHITContent" style="display:none;">
+				<p>
+					Generate the HTML code to paste into your HIT. Full instructions are on the <a href="http://gideongoldin.github.com/TurkGate/" target="blank">TurkGate Wiki</a>.
+				</p>
 						
-			<?php
-					$textAreaId = 'generatedHTMLCode';								
-					require_once 'lib/autoselect.php';
-            ?>								
-			<div id="generatedContent">
-				<em><small>Copy and paste the code below into the source code for your HIT:</small></em>
-				<textarea rows="8" style="height:80;" id="<?php echo $textAreaId; ?>"></textarea>
+				<?php
+						$textAreaId = 'generatedHTMLCode';								
+						require_once 'lib/autoselect.php';
+	            ?>								
+				<div id="generatedContent">
+					<em><small>Copy and paste the code below into the source code for your HIT:</small></em>
+					<textarea rows="8" style="height:80;" id="<?php echo $textAreaId; ?>"></textarea>
+				</div>
 			</div>
+
+			<div id="CLTHITContent" style="display:none;">
 
 				<p>
 					Download the files for creating your HIT. Full instructions are on the <a href="https://github.com/gideongoldin/TurkGate/wiki/Command-Line-Tools" target="blank">TurkGate Wiki</a>.
@@ -134,27 +99,21 @@
 				<button id="downloadCLTPropertiesFile">survey.properties</button>
 				<button id="downloadCLTQuestionFile">survey.question</button>
 
-			<!-- Completion code stuff -->
-			<p class="new-section">
-				<span class="section-begin">Provide completion codes</span> to your participants at the end of your survey by sending them to one of the following URLs:
-			</p>
-				<?php
-				    $numDigits = 6;
-					$generateCodeLink = constant('BASE_URL') . '/codes/generate.php?stamp=' . mt_rand(pow(10, $numDigits), pow(10, $numDigits + 1) - 1);	
-				?>
-			<dl>
-				<dt>For Qualtrics:</dt>
-					<dd><a href="#"><?php echo $generateCodeLink ?>&responseID=${e://Field/ResponseID}</a></dd>
-						
-					<dt>For LimeSurvey:</dt>
-					<dd><a href="#"><?php echo $generateCodeLink ?>&user={SAVEDID}&survey={SID}</a></dd>	
-						
-					<dt>For other sites:</dt>
-					<dd><a href="#"><?php echo $generateCodeLink ?></a></dd>
-				</dl>
-			<p class="new-section">
-				<span class="section-begin">Verify completion codes</span> at <a href="codes/verify.php">this page</a>.
-			</p>
+			</div>
+
+			<div id="CompletionContent" style="display:none;">
+				<p class="new-section">
+					<span class="section-begin">Provide completion codes</span> to your participants at the end of your survey by sending them to the following URL:
+				</p>
+
+				<p>
+					<a href="#" target="_blank" id="completionLink"></a>
+				</p>
+
+				<p>
+					<span class="section-begin">Verify completion codes</span> at <a href="codes/verify.php">the verification page</a>.
+				</p>
+			</div>
 		</div> <!-- columns omega -->
 		
 	</div> <!-- columns clearfix -->
@@ -163,10 +122,11 @@
 
 <!-- Custom jQuery actions -->
 <script type="text/javascript">
-	$(document).ready(function(){ 
-		// CLT Variables
-		
 
+	var isAnimating = false;
+	var alreadySubmitted = false;
+
+	$(document).ready(function(){ 
 
 		// Setup tabs
 		$(".tab_content").hide();
@@ -193,7 +153,9 @@
       			populateAssociatedURLs(ui.item.value);
       		},
       		change: function(event, ui) { 
-      			populateAssociatedURLs(ui.item.value);
+      			if(ui.item != null) {
+      				populateAssociatedURLs(ui.item.value);
+      			}
       		}
       	});
 
@@ -205,64 +167,119 @@
           $('#externalSurveyURL').val(fix_http($('#externalSurveyURL').val()));
         });
         
-		// each of the submission buttons sets the function to be called upon submission, but doesn't 
-		// actually call the function. Only the submit event triggers the function, allowing it to check 
-		// that all required fields have been filled out.
-        $('#generateHTMLCode').click( function () { 
-        	$('#hitGenerationForm').data("submitFunction", generateWebCode); 
-        });
-        
-        $('#downloadCLTInputFile').click( function () { 
-        	$('#hitGenerationForm').data("submitFunction", createDownloadHandler('survey.input')); 
-        });
-        
-        $('#downloadCLTPropertiesFile').click( function () { 
-        	$('#hitGenerationForm').data("submitFunction", createDownloadHandler('survey.properties')); 
-        });
-        
-        $('#downloadCLTQuestionFile').click( function () { 
-        	$('#hitGenerationForm').data("submitFunction", createDownloadHandler('survey.question')); 
-        });
-        
-        $('#generatedContent').hide();
-        
         // submitting the form calls whatever function has been set
         $('#hitGenerationForm').submit(function () { 
-        	var subFunc = $('#hitGenerationForm').data("submitFunction");
-        	if (typeof subFunc === 'function') {
-          		subFunc();
-          	}
-        
-        	$('#hitGenerationForm').removeData("submitFunction");
 
-        	/*
-        	layout slideing animation
-        	*/
+    		if (isAnimating) {
+    			return false;
+    		}
+        	isAnimating = true;
 
-        	//$("#rightColumnMain").show("slide", { direction: "left" }, 1000);
-        	$("#rightColumnMain").animate({left:'+=460px'},'slow');   
+        	if (alreadySubmitted) {
+ 		      	$("#rightColumnMain").animate(
+	 		      		{
+	 		      			left:'-=460px'
+	 		      		},
+	 		      		'fast', 
+	 		      		function() {
+							resetHITContent();
+			 		      	populateHITContent();
+	 		      		}
+ 		      		);   
+       		} else {
+       			alreadySubmitted = true;
+       			populateHITContent();
+       		}
 
           	return false;
         });
+
     });
 
+	function resetHITContent() {
+		$("#WebHITContent").hide();
+		$("#CLTHITContent").hide();
+		$("#CompletionContent").hide();
+
+		$("#downloadCLTInputFile").unbind("click");
+    	$("#downloadCLTPropertiesFile").unbind("click");
+    	$("#downloadCLTQuestionFile").unbind("click");
+	}
+
+	function populateHITContent() {
+		var surveyURL = fix_http($('#externalSurveyURL').val());
+		var groupName = $('#groupName').val();
+
+    	switch ($("#HITType").val()) {
+    		case "WebInterface":
+    			generateWebCode(surveyURL, groupName);
+    			$("#WebHITContent").show();
+    			break;
+    		case "CLT":
+    			$("#downloadCLTInputFile").click(function() { downloadCLTFile("survey.input", surveyURL, groupName); });
+    			$("#downloadCLTPropertiesFile").click(function() { downloadCLTFile("survey.properties", surveyURL, groupName); });
+    			$("#downloadCLTQuestionFile").click(function() { downloadCLTFile("survey.question", surveyURL, groupName); });
+    			$("#CLTHITContent").show();
+    			break;
+    		default:
+    			alert("ERROR: Invalid HIT Type.");
+    			return false;
+    	}
+
+
+    	// Generate completion link
+    	var numDigits = 6;
+    	var completionLink = "<?php echo constant('BASE_URL') ?>codes/generate.php?stamp=" + Math.floor(Math.random() * Math.pow(10, numDigits) + Math.pow(10, numDigits + 1)); 
+    	switch($("#surveySites").val()) {
+    		case "LimeSurvey":
+    			completionLink += "&user={SAVEDID}&survey={SID}";
+    			break;
+    		case "Qualtrics":
+    			completionLink += "&responseID=${e://Field/ResponseID}";
+    			break;
+    		case "Other":
+    			// do nothing
+    			break;
+    		default:
+    			alert("ERROR: Invalid Survey Site.");
+    			return false;
+    	}
+    	$("#completionLink").html(completionLink);
+    	$("#completionLink").attr('href', completionLink );
+    	$("#CompletionContent").show();
+    	
+    	$("#rightColumnMain").animate(
+    		{
+    			left:'+=460px'
+    		},
+    		'slow',
+    		function() {
+    			isAnimating = false;
+    		}
+    	);   
+	}
+
 	function populateAssociatedURLs(groupName) {
-			$.ajax({                                      
-      			url: 'lib/fetchsurveyurls.php',                        
-      			data: "group="+groupName,
-      			dataType: 'json', 
-      			success: function(data) {
-      				$("#associatedURLs").val("");
+		$.ajax({                                      
+  			url: 'lib/fetchsurveyurls.php',                        
+  			data: "group="+groupName,
+  			dataType: 'json', 
+  			success: function(data) {
+  				// Reset box contents
+  				$("#associatedURLs").val("");
+
+  				if(data.length == 0) {
+  					$("#associatedURLs").val("No surveys were run in this group yet.");
+  				} else {
 			        for (var i=0; i<data.length; i++) {
 			        	$("#associatedURLs").val($("#associatedURLs").val() + data[i].value + "\n");
 			        }
-			    } 
-    		});
+		    	}
+		    } 
+		});
 	}
 
-	function generateWebCode() {
-		var surveyURL = fix_http($('#externalSurveyURL').val());
-		var groupName = $('#groupName').val();
+	function generateWebCode(surveyURL, groupName) {
 		var copyright = "<!-- Copyright (c) 2012 Adam Darlow and Gideon Goldin. For more info, see http://gideongoldin.github.com/TurkGate/ -->\n"; 
 		
 		var htmlCode = <?php echo json_encode(file_get_contents('resources/WebHIT/webTemplate.html')); ?>;
@@ -272,16 +289,13 @@
 		htmlCode = htmlCode.replace(/<!--[^>]*-->/, copyright);
 		
 		$('#generatedHTMLCode').val(htmlCode);
-		$('#generatedContent').slideDown();
 	}
 	
 	function endsWith(str, suffix) {
         return str.indexOf(suffix, str.length - suffix.length) !== -1;
     }
 	
-	function downloadCLTFile(fileName) {
-		var surveyURL = fix_http($('#externalSurveyURL').val());
-		var groupName = $('#groupName').val();
+	function downloadCLTFile(fileName, surveyURL, groupName) {
 
 		var url = 'lib/downloadFile.php?file=' + encodeURIComponent(fileName);
 		
