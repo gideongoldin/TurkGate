6c06ca477e153030f2f3802940498a185a98c0c5
Gideon Goldin <gideongoldin@gmail.com>
2013-01-31 14:35:53 -0500

    Updated citation formatting.

--- TurkGate/includes/footer.php
+++ TurkGate/includes/footer.php
@@ -4,13 +4,17 @@
 					<h6>
 					  Powered by <a href='http://gideongoldin.github.com/TurkGate/' target="_blank">TurkGate</a> (version 0.3.2)
 					  <br />
-					  Copyright &copy; Adam Darlow and Gideon Goldin. All rights reserved.
+					  Copyright &copy; Adam Darlow and Gideon Goldin.
+					  <br />All rights reserved.
 					</h6>	
 				</div>
 				<div class="eight columns omega">
 					<h6>
-						Goldin, G., Darlow, A. (2013). TurkGate (Version 0.3.2) [Software]. Available from http://gideongoldin.github.com/TurkGate/
+						Please cite TurkGate as:
 					</h6>
+					<code>
+						Goldin, G., Darlow, A. (2013). TurkGate (Version 0.3.2) [Software]. Available from http://gideongoldin.github.com/TurkGate/
+					</code>
 				</div>
 				
 			</div> <!-- sixteen columns clearfix -->
