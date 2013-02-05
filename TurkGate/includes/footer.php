9a387e3ebeb6ff2651c4ca8ef4d749945ae2c569
Gideon Goldin <gideongoldin@gmail.com>
2013-02-05 13:53:04 -0500

    Updated version information in footer.

--- TurkGate/includes/footer.php
+++ TurkGate/includes/footer.php
@@ -2,7 +2,7 @@
 			<div class="sixteen columns clearfix" id="footerDiv">
 				<div class="eight columns alpha">
 					<h6>
-					  Powered by <a href='http://gideongoldin.github.com/TurkGate/' target="_blank">TurkGate</a> (version 0.3.2)
+					  Powered by <a href='http://gideongoldin.github.com/TurkGate/' target="_blank">TurkGate</a> (version 0.4.0)
 					  <br />
 					  Copyright &copy; Adam Darlow and Gideon Goldin.
 					  <br />All rights reserved.
